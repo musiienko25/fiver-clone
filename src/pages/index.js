fd3cb8f37721171aa333be39ce51b1e3fd9c2619
musiienko25 <mysterious240@gmail.com>
2023-06-30 17:30:24 +0300

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div className="text-red-600">test</div>
+      <div className="text-red-600">testfff</div>
     </main>
   );
 }
