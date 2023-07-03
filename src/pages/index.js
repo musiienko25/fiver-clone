be274b640c390870673b48b8dbbee18219eb4d0c
musiienko25 <mysterious240@gmail.com>
2023-07-03 18:10:32 +0300

    landing pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,9 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Footer from "@/components/Footer";
 import HomeBanner from "@/components/Landing/HomeBanner";
+import FiverrBusiness from "@/components/Landing/FiverBusiness";
+import Everything from "@/components/Landing/Everything";
+import JoinFiver from "@/components/Landing/JoinFiver";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +14,10 @@ export default function Home() {
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <HomeBanner />
-      <Footer />
+      {/* <Footer /> */}
+      {/* <FiverrBusiness /> */}
+      {/* <Everything /> */}
+      <JoinFiver />
     </main>
   );
 }
