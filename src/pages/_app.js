6ead3fb574e29765ff56a9ec924f660be22d153d
musiienko25 <mysterious240@gmail.com>
2023-07-19 00:04:03 +0300

    overwite mane page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,11 +3,44 @@ import { StateProvider } from "@/context/StateContext";
 import reducer, { initialState } from "@/context/StateReducers";
 import "@/styles/globals.css";
 
+import Footer from "../components/Footer";
+
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useCookies } from "react-cookie";
+import { useEffect } from "react";
+
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  const [cookies] = useCookies();
+  useEffect(() => {
+    if (
+      router.pathname.includes("/seller") ||
+      router.pathname.includes("/buyer")
+    ) {
+      if (!cookies.jwt) {
+        router.push("/");
+      }
+    }
+  }, [cookies, router]);
+
   return (
     <StateProvider initialState={initialState} reducer={reducer}>
-      <Navbar />
-      <Component {...pageProps} />
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <title>Fiverr Clone</title>
+      </Head>
+      <div className="relative flex flex-col h-screen justify-between">
+        <Navbar />
+        <div
+          className={`${
+            router.pathname !== "/" ? "mt-36" : ""
+          } mb-auto w-full mx-auto`}
+        >
+          <Component {...pageProps} />
+        </div>
+        <Footer />
+      </div>
     </StateProvider>
   );
 }
